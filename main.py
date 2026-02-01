16251b0187cb46d61aa65ef22bc9ec29b9069cf1
Arshique <alamarshique9@gmail.com>
2026-02-01 22:09:51 +0530

    feat: support multiple exit keywords

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from core.brain import process_input
 
 print("JARVIS is online. Type 'exit' to stop.")
 
+exit_commands = {"exit", "quit", "shutdown", "stop", "bye", "goodbye", "see you"}
+
 while True:
     user_input = input("You: ").lower()
 
-    if user_input == "exit":
+    if user_input in exit_commands:
         print("JARVIS: Shutting down.")
         break
 
